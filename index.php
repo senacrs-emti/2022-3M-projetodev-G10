6a69483e70efced24ec57ba9f0b2b41ff45c519d
Riann <60112780008@senacrs.edu.br>
2022-09-26 10:53:54 -0300

    .

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Home</a>
-          <a class="navbar-brand" href="music-player/index.html">Ouvir Música</a>
+          <a class="navbar-brand" href="music-player/index.php">Ouvir Música</a>
             </div>
           </div>
         </div>
