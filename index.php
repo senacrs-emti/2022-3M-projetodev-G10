7d17b0fc75c5ea7b2223ea85739844a3b310bce6
Luizin27 <103116444+Luizin27@users.noreply.github.com>
2022-09-28 08:57:19 -0300

    imagem na index.

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="music-player/index.php">Ouvir Música</a>
+          <a class="navbar-brand" href="music-player/index.php">Ouvir música</a>
             </div>
           </div>
         </div>
       </nav>
 </header>
+
+<img src="https://a-static.mlcdn.com.br/800x560/bateria-infantil-banco-baqueta-rock-star-prato-chimbal-com-baquetas-iniciante-instrumento-brinquedo-toy-brow/mundodocomercio/15831158100/bb6658d6d771c13809518f7d89bdd198.jpg" alt="Minha bateria!" title="Batera braba!!!" width="300px" height="300px" vspace="50px" hspace=" 40px" align="left"/>
 
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
