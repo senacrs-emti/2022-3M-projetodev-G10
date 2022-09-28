353ef903555a6bfb289864d8fa0a8695a34cd264
HallsRaul <84603491+riannrodrigues@users.noreply.github.com>
2022-09-28 08:36:01 -0300

    home index

--- music-player/index.php
+++ music-player/index.php
@@ -16,7 +16,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Home</a>
+          <a class="navbar-brand" href="../index.php">Home</a>
           <a class="navbar-brand" href="music-player/index.php">Ouvir Música</a>
             </div>
           </div>
