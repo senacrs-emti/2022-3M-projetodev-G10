4cc244e42654f89ac2c1b47273b59c6b7e1d7859
Luizin27 <103116444+Luizin27@users.noreply.github.com>
2022-10-03 12:15:25 -0300

    bateria

---
+++ bateria.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="pt" class="no-js">
+<head>
+<meta name="viewport" content="width=device-width,user-scalable=1,initial-scale=1.0,minimum-scale=1.0" />
+<meta property="og:url" content="https://www.musicca.com/pt/bateria" />
+<meta property="og:title" content="Bateria virtual – Toca bateria online | Musicca" />
+<meta property="og:site_name" content="Musicca" />
+<meta property="og:image" content="https://www.musicca.com/images/facebook-image.png" />
+<link rel="canonical" href="https://www.musicca.com/pt/bateria" />
+<base href="https://www.musicca.com/pt/bateria" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="/modules/mod_ads/css/style.css?v=1664808718" rel="stylesheet" type="text/css" />
+<script type="application/json" class="joomla-script-options new">{"joomla.jtext":{"ERROR":"Erro"},"mod_ads":{"url":"https:\/\/cdn.fuseplatform.net\/publift\/tags\/2\/2548\/fuse.js"},"csrf.token":"028c12ca51148cfd168e5a6d65dca16a","system.paths":{"root":"","base":""}}</script>
+<script src="/media/jui/js/jquery.min.js?d1099a688fe8068dee368f9cd86a2d34"></script>
+<script src="/modules/mod_ads/js/width_test.js?v=1664808718"></script>
+<script src="/media/system/js/core.js?d1099a688fe8068dee368f9cd86a2d34"></script>
+<script>
+jQuery(window).on('load',  function() {
+				
+			});
+</script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="/templates/musicca/css/screen.min.css?v=1664808718">
+<link rel="icon" type="image/png" href="/templates/musicca/images/favicon.png" />
+<script defer src="/scripts/tooltip/opentip-native-excanvas.min.js"></script>
+<script defer src="/templates/musicca/js/app.min.js?v=1664808718"></script>
+
+<style>
+.toggle-menu-mobile span > span{
+	position: absolute;
+	height: 1px;
+	width: 1px;
+	clip: rect(1px, 1px, 1px, 1px);
+	padding: 0;
+	border: 0;
+	overflow: hidden;	
+}
+
+.header-content .navigation__lang .lang-menu{
+	overflow-x: hidden;
+}
+
+</style>
+<noscript>
+
+<style>
+.navigation__top {
+    z-index: 9;
+}
+
+.navigation__mobile,
+
+.fix__content{
+    position: relative;
+}
+
+.mobile_menu__toggle{
+	display: none;
+}
+
+.mobile__menu{
+    display: block;
+}
+
+li.mobile_menu__item:hover ul {
+    display: block;
+}
+.header-content .navigation__lang .lang_button .current-lang{
+	cursor: auto;
+}
+
+.header-content .navigation__lang .lang_button .current-lang:hover > .sprite {
+    opacity: 1;
+}
+
+</style>
+</noscript>
+
+<script>
+									(function() {
+										// ---------Header menu highlight active item--------
+
+										var sideBarHeaderLink = jQuery('.navigation__left .left-nav a');
+
+										if (sideBarHeaderLink.length) {
+											var headerMenuLinks = jQuery('.navigation__top .menu__item .top_level');
+
+											if (headerMenuLinks.length) {
+												for (var i = 0; i < headerMenuLinks.length; i++) {
+													headerMenuLinks[i].innerHTML == sideBarHeaderLink.text() ?
+													headerMenuLinks[i].parentElement.classList.add('active') :
+													headerMenuLinks[i].parentElement.classList.remove('active');
+												}
+											}
+										}
+										// ---------End Header menu highlight active item--------
+
+									})();
+
+								</script>
+
+<div>
+<h1 class="h1 default__page__title" itemprop="name">
+Bateria virtual </h1>
+</div>
+
+
+<div itemprop="articleBody">
+<div class="default__page__content">
+<link href="/files/scripts/drumkit/static/css/main.chunk.css" rel="stylesheet" />
+<script>
+  window.translation = {
+    shortcuts_button: {
+      show: "Mostrar atalhos",
+      hide: "Esconder atalhos"
+    },
+    keyCodes: {
+      // an additional shortcuts can be assigned along with the existed
+      keys: {
+        87: "hihatLeft",
+        69: "hihatRight",
+        82: 'hihatLeftClosed',
+        84: 'hihatRightClosed',
+        67: 'hihatFoot',
+        89: 'crash',
+        85: 'ride',
+        65: 'snareLeft',
+        83: 'snareRight',
+        68: 'snareLeftCrossStick',
+        70: 'snareRightCrossStick',
+        88: 'bass',
+        90: 'bass',
+        71: 'tom1',
+        72: 'tom2',
+        74: 'floor',
+        73: 'snareRight' // an example of additional shortcut
+      },
+      names: {
+        hihatRight: 'E',
+        hihatRightClosed: 'R',
+        hihatFoot: 'C',
+        crash: 'Y',
+        ride: 'U',
+        snareRight: 'S',
+        snareRightCrossStick: 'D',
+        bass: 'X',
+        tom1: 'G',
+        tom2: 'H',
+        floor: 'J',
+      }
+    }
+  }
+</script>
+<div id="drum-kit"></div>
+<script src="/files/scripts/drumkit/static/js/2.chunk.js"></script>
+<script src="/files/scripts/drumkit/static/js/3.chunk.js"></script>
+<script src="/files/scripts/drumkit/static/js/main.chunk.js"></script>
+<script src="/files/scripts/drumkit/static/js/runtime-main.js"></script>
+<div class="tools__page__content">
+
+</article>
+<div class="after__content">
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="music-player/index.php">Ouvir música</a>
+          <a class="navbar-brand" href="bateria.php">Tocar bateria</a>
             </div>
           </div>
         </div>
@@ -15,20 +16,8 @@
 <img src="https://a-static.mlcdn.com.br/800x560/bateria-infantil-banco-baqueta-rock-star-prato-chimbal-com-baquetas-iniciante-instrumento-brinquedo-toy-brow/mundodocomercio/15831158100/bb6658d6d771c13809518f7d89bdd198.jpg" alt="Minha bateria!" title="Batera braba!!!" width="300px" height="300px" vspace="50px" hspace=" 40px" align="left"/>
 <h1 style="color: white; margin-top:50px; font-size: 25px; height:200px; display: flex;";>Olá, sejam bem-vindos ao nosso site. Aqui você poderá desfrutar das nossas baterias "eletrônicas", já que você é pobre e não tem dinheiro para comprar uma :P. </h1>
 
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
-<p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem </p>
 
 
-<?php 
-  include_once "includes/footer.php";
-?>
 <script>
   function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
@@ -50,6 +39,10 @@
   const titulo = document.querySelector('h1');
   typeWriter(titulo);
 </script>
+
+<?php 
+  include_once "includes/footer.php";
+?>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
